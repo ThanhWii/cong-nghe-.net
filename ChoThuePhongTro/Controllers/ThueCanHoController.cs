df8911f3cec774f1e8da85e8300fb8c1d15135e0
ThanhWii <huynguyen2kk3@gmail.com>
2024-10-28 12:55:19 +0700

    con loi trang chu

---
+++ ChoThuePhongTro/Controllers/ThueCanHoController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChoThuePhongTro.Controllers
+{
+    public class ThueCanHoController : Controller
+    {
+        // GET: ThueCanHo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
